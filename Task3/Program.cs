3211cea79a685ac82f258b223be703b8a0e55ea0
AnnaMikshina <wow154@mail.ru>
2023-02-11 01:04:44 +0500

    Задача 3

---
+++ Task3/Program.cs
@@ -0,0 +1,56 @@
+﻿// //Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+int Prompt(string message)
+{
+    Console.Write($"{message}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int m = Prompt("Введите количество строк в массиве");
+int n = Prompt("Введите количество столбцов в массиве");
+
+int[,] GenerateArray(int row, int col)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] array)
+{
+    int row = array.GetLength(0);
+    int col = array.GetLength(1);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+int[,] array = GenerateArray(m, n);
+PrintArray(array);
+System.Console.WriteLine();
+Console.Write($"Среднее арифметическое каждого столбца:");
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    Console.Write($"{ sum / array.GetLength(0)}; ");
+}
